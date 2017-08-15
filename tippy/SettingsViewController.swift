0ea4faba74725cf58ac7cdf2f56e4da8495162ef
Ester Bogod <ebogod@mix.com>
2017-08-15 08:26:51 -0700

    First tippy commit

---
+++ tippy/SettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsViewController.swift
+//  tippy
+//
+//  Created by Ester Bogod on 8/14/17.
+//  Copyright © 2017 Ester Bogod. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var defaultTipControl: UISegmentedControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let defaults = UserDefaults.standard
+        let selection = defaults.integer(forKey: "default_tip_selection")
+        defaultTipControl.selectedSegmentIndex = selection
+    }
+
+    @IBAction func tipDefaultChanged(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(defaultTipControl.selectedSegmentIndex, forKey: "default_tip_selection")
+        defaults.synchronize()
+    }
+}
---
+++ tippy/TipViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Ester Bogod on 8/10/17.
+//  Copyright © 2017 Ester Bogod. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField! {
+        didSet {
+             billField.attributedPlaceholder = NSAttributedString(string: billField.placeholder!, attributes: [NSForegroundColorAttributeName : UIColor.white])
+        }
+    }
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var valuesView: UIView!
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let bill = Double(billField.text!) ?? 0
+        valuesView.alpha = (bill == 0) ? 0 : 1
+        let defaults = UserDefaults.standard
+        let selection = defaults.integer(forKey: "default_tip_selection")
+        tipControl.selectedSegmentIndex = selection
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+
+        let bill = Double(billField.text!) ?? 0
+        UIView.animate(withDuration: 0.2, animations: {
+            self.valuesView.alpha = (bill == 0) ? 0 : 1
+        })
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+}
+
